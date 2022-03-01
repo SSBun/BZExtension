9a138d9834f3b0c82bc15aff121d9a900bfc0bd6
caishilin <caishilin@zhihu.com>
2022-03-01 13:27:44 +0800

    Initialize

---
+++ Sources/BZExtension/BZExtension.swift
@@ -0,0 +1,6 @@
+public struct BZExtension {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/BZExtensionTests/BZExtensionTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BZExtension
+
+final class BZExtensionTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BZExtension().text, "Hello, World!")
+    }
+}
